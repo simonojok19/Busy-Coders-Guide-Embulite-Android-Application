a24e6fd644a26791ad6b163a0bae528785187897
simonojok19 <simonojok19@gmail.com>
2019-08-08 16:48:57 +0300

    added load notes thread in DatabaseHelper class

--- app/src/main/java/com/commonsware/empublite/DatabaseHelper.java
+++ app/src/main/java/com/commonsware/empublite/DatabaseHelper.java
@@ -2,8 +2,12 @@ package com.commonsware.empublite;
 
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Process;
+
+import org.greenrobot.eventbus.EventBus;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "empulite.db";
@@ -32,5 +36,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         throw new RuntimeException("This should not be called");
 
+    }
+
+    void loadNote(int position){
+        new LoadThread(position).start();
+    }
+
+    private class LoadThread extends Thread {
+        private int position = -1;
+
+        LoadThread(int position){
+            super();
+            this.position = position;
+        }
+
+        @Override
+        public void run() {
+            Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+            String[] args = {String.valueOf(position)};
+            Cursor c = getReadableDatabase().rawQuery("SELECT prose FROM notes WHERE position = ?",
+                    args);
+
+            if ( c.getCount() > 0){
+                c.moveToFirst();
+            }
+
+            EventBus.getDefault().post(new NoteLoadedEvent(position, c.getString(0)));
+
+            c.close();
+        }
     }
 }
