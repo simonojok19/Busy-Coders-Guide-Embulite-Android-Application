19f672181ee0e2e8d95a97e807e77e98d1a3a819
simonojok19 <simonojok19@gmail.com>
2019-08-08 13:40:05 +0300

    added ModelFragment for loading data and performing file IO

--- app/src/main/java/com/commonsware/empublite/ModelFragment.java
+++ app/src/main/java/com/commonsware/empublite/ModelFragment.java
@@ -2,73 +2,66 @@ package com.commonsware.empublite;
 
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.app.Fragment;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.os.Process;
-import android.app.Fragment;
-import android.preference.PreferenceManager;
 import android.util.Log;
+
 import com.google.gson.Gson;
+
 import org.greenrobot.eventbus.EventBus;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.concurrent.atomic.AtomicReference;
 
+
 // holding data bout the books
 public class ModelFragment extends Fragment {
     final private AtomicReference<BookContents> contents = new AtomicReference<>();
-    final private AtomicReference<SharedPreferences> prefs = new AtomicReference<>();
-
-    public ModelFragment() {
-        // Required empty public constructor
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
     }
 
-
     @Override
-    public void onAttach(Activity host) {
+    public void onAttach(Activity host){
         super.onAttach(host);
-        if(contents.get() == null){
-//            is there contents in AtomicReference no start thread to load
-            new LoadThread(host).start();
+
+        if (contents.get() == null) {
+            new LoadThread(host.getAssets()).start();
         }
     }
 
     public BookContents getBook(){
-        return contents.get();
+        return (contents.get());
     }
 
-    public SharedPreferences getPrefs(){
-        return prefs.get();
-    }
-
-    private class LoadThread extends Thread {
-        final private Context ctxt;
-        LoadThread(Context ctxt){
+    private class LoadThread extends Thread{
+        private AssetManager assetManager = null;
+        LoadThread(AssetManager assetManager) {
             super();
-            this.ctxt = ctxt.getApplicationContext();
+            this.assetManager = assetManager;
         }
 
         @Override
-        public void run() {
-            prefs.set(PreferenceManager.getDefaultSharedPreferences(ctxt));
+        public void run(){
             Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
             Gson gson = new Gson();
+
             try {
-                InputStream is = ctxt.getAssets().open("book/contents.json");
+                InputStream is = assetManager.open("book/contents.json");
                 BufferedReader reader = new BufferedReader(new InputStreamReader(is));
                 contents.set(gson.fromJson(reader, BookContents.class));
                 EventBus.getDefault().post(new BookLoadedEvent(getBook()));
-            } catch (IOException e){
-                Log.e(getClass().getSimpleName(), "Exception parsing JSON", e);
+
+            } catch (IOException e) {
+                Log.e(getClass().getSimpleName(),"Exception parsing JSON" + e);
             }
         }
     }
