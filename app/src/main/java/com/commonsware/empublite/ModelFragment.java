2a11af4170504acf85c93c6e5d30bb3a0f50e0dc
simonojok19 <simonojok19@gmail.com>
2019-08-08 13:07:20 +0300

    reviewed ModelFragment

--- app/src/main/java/com/commonsware/empublite/ModelFragment.java
+++ app/src/main/java/com/commonsware/empublite/ModelFragment.java
@@ -17,6 +17,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.concurrent.atomic.AtomicReference;
 
+// holding data bout the books
 public class ModelFragment extends Fragment {
     final private AtomicReference<BookContents> contents = new AtomicReference<>();
     final private AtomicReference<SharedPreferences> prefs = new AtomicReference<>();
