65b736db390f5fcdb2887a4095aee256ed8df3f6
simonojok19 <simonojok19@gmail.com>
2019-08-08 13:14:26 +0300

    added BookContent

--- app/src/main/java/com/commonsware/empublite/BookContents.java
+++ app/src/main/java/com/commonsware/empublite/BookContents.java
@@ -3,22 +3,27 @@ package com.commonsware.empublite;
 import java.util.List;
 
 public class BookContents {
+
+    static class Chapter {
+        String file;
+        String title;
+    }
+
+
     List<BookContents.Chapter> chapters;
 
     int getChapterCount() {
         return chapters.size();
     }
 
-    String getChapterFile(int position){
-        return chapters.get(position).file;
-    }
+
 
     String getChapterTitle(int position){
         return chapters.get(position).title;
     }
 
-    static class Chapter {
-        String file;
-        String title;
+    String getChapterFile(int position){
+        return chapters.get(position).file;
     }
+
 }
