97039d55bab6f578cfa027683f9e456576dce31b
simonojok19 <simonojok19@gmail.com>
2019-08-08 13:16:30 +0300

    added bookloaded event

--- app/src/main/java/com/commonsware/empublite/BookLoadedEvent.java
+++ app/src/main/java/com/commonsware/empublite/BookLoadedEvent.java
@@ -3,11 +3,12 @@ package com.commonsware.empublite;
 public class BookLoadedEvent {
     private BookContents contents = null;
 
-    public BookLoadedEvent(BookContents contents){
-        this.contents = contents;
-    }
+
 
     public BookContents getBook() {
         return contents;
+    }
+    public BookLoadedEvent(BookContents contents){
+        this.contents = contents;
     }
 }
