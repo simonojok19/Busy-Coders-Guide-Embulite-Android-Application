cd52836078661fe163f306e5f165c5157d14a07a
simonojok19 <simonojok19@gmail.com>
2019-08-10 13:46:53 +0300

    added interface for retofit2

--- app/src/main/java/com/commonsware/empublite/BookUpdateInterface.java
+++ app/src/main/java/com/commonsware/empublite/BookUpdateInterface.java
@@ -1,4 +1,9 @@
 package com.commonsware.empublite;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface BookUpdateInterface {
+    @GET("/msc/empublite-update.json")
+    Call<BookUpdateInfo> update();
 }
