0ec2989f65c890adc7d826129977d134382bc082
simonojok19 <simonojok19@gmail.com>
2019-08-10 14:34:59 +0300

    added method getUpdateUrl in service

--- app/src/main/java/com/commonsware/empublite/DownloadCheckService.java
+++ app/src/main/java/com/commonsware/empublite/DownloadCheckService.java
@@ -4,8 +4,14 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 
+import java.io.IOException;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class DownloadCheckService extends IntentService {
+    private static final String OUR_BOOK_DATE = "20120418";
 
     public DownloadCheckService(String name) {
         super(name);
@@ -15,4 +21,21 @@ public class DownloadCheckService extends IntentService {
     protected void onHandleIntent(@Nullable Intent intent) {
 
     }
+
+
+
+   private String getUpdateUrl() throws IOException {
+       Retrofit retrofit = new Retrofit.Builder()
+               .baseUrl("https://commonsware.com")
+               .addConverterFactory(GsonConverterFactory.create())
+               .build();
+
+       BookUpdateInterface updateInterface = retrofit.create(BookUpdateInterface.class);
+       BookUpdateInfo info = updateInterface.update().execute().body();
+       if ( info.updatedOn.compareTo(OUR_BOOK_DATE) > 0){
+           return info.updateUrl;
+       }
+
+       return null;
+   }
 }
